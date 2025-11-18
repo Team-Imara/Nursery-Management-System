d22fcd9e8e29924d143b15bc07ec7417a8c2654f
Nuha <aayshanuha633@gmail.com>
2025-11-18 16:24:59 +0530

    New changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,12 @@ import AddTeacher from './layout/AddTeacher.jsx';
 import ManageLeaveRequests from './layout/ManageLeaveRequests.jsx';
 import Dashboard from "./layout/Dashboard.jsx";
 import LoginPage from "./layout/LoginPage.jsx";
+import Students from './layout/Students.jsx';
+import StudentDetail from './layout/StudentDetail.jsx';
+import AddStudent from './layout/AddStudent.jsx';
+import SendMessage from './components/SendMessage.jsx';
+import EventManagement from './layout/EventManagement';
+import EventDetail from './layout/EventDetail.jsx';
 
 function App() {
     return (
@@ -16,6 +22,14 @@ function App() {
                 <Route path="/teacher/:id" element={<TeacherDetail />} />
                 <Route path="/add-teacher" element={<AddTeacher />} />
                 <Route path="/manage-leave-requests" element={<ManageLeaveRequests />} />
+                <Route path="/students" element={<Students />} />
+                {/* <Route path="/student/:id" element={<StudentDetail />} /> */}
+                <Route path="/students/detail" element={<StudentDetail />} />
+                <Route path="/add-student" element={<AddStudent />} />
+                <Route path="/send-message" element={<SendMessage />} />
+                <Route path="/event-management" element={<EventManagement />} />
+                <Route path="/event/:date" element={<EventDetail />} />
+
             </Routes>
         </Router>
     );
